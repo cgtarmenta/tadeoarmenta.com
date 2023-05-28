2add419330163c241ed1edb7e6f1d051d7cf2c8b
Tadeo Armenta <tadeo.armenta@servita.com>
2023-05-28 17:03:12 +0200

    Fix build

--- src/components/ui/LabeledCollapsible.vue
+++ src/components/ui/LabeledCollapsible.vue
@@ -1,11 +1,11 @@
 <template>
   <section :class="['flex flex-col w-full p-2.5 rounded-md', border ? borderClass : '']">
-    <slot name="header" :show="show" :toggle="toggle" :title-class="titleSize" :always-on="alwaysOn">
+    <slot name="header" :show="show" :toggle="toggle" :title-class="size" :always-on="alwaysOn">
       <a class="flex flex-row flex-grow w-full justify-start items-center space-x-4 cursor-pointer" @click.prevent="alwaysOn ? null : toggle()">
         <span v-if="show"> <SolidArrowDownIcon class="h-2 w-2 " transform="rotate(180 0,0)" /></span>
         <span v-else> <SolidArrowDownIcon class="h-2 w-2 " /></span>
         <slot name="title">
-          <span :class="[titleSize]">
+          <span :class="[size]">
             {{ title }}
           </span>
         </slot>
@@ -23,7 +23,7 @@
 </template>
 
 <script lang="ts" setup>
-import {computed, ref} from "vue";
+import {ref} from "vue";
 import SolidArrowDownIcon from "@/components/icons/SolidArrowDownIcon.vue";
 const props = withDefaults(defineProps<{
   alwaysOn?: boolean
@@ -40,22 +40,6 @@ const props = withDefaults(defineProps<{
 })
 
 const show = ref(props.alwaysOn)
-const titleSize = computed(() => {
-  const configSizes = {
-    'base': 'text-base',
-    'lg': 'text-lg',
-    'xl': 'text-xl',
-    '2xl': 'text-2xl',
-    '3xl': 'text-3xl',
-    '4xl': 'text-4xl',
-    '5xl': 'text-5xl',
-    '6xl': 'text-6xl',
-    '7xl': 'text-7xl',
-    '8xl': 'text-8xl',
-    '9xl': 'text-9xl',
-  };
-  return configSizes[props.size as string];
-})
 const toggle = () => {
   show.value = !show.value
 }
