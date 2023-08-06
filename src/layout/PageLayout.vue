eb4fe9352288b520a1d3dc35accda2b199481772
Tadeo Armenta <tadeo.armenta@servita.com>
2023-08-06 16:31:28 +0200

    Wip About me

--- src/layout/PageLayout.vue
+++ src/layout/PageLayout.vue
@@ -1,5 +1,8 @@
 <template>
   <article class="flex flex-row h-full max-w-full max-h-fit overflow-hidden my-2 px-4">
+    <section class="flex flex-col border-r border-lines w-16.5 min-w-[68px] h-full">
+      <slot name="task" />
+    </section>
     <section class="flex flex-col border-r border-lines w-72 min-w-[311px] h-full">
       <slot name="menu" />
     </section>
